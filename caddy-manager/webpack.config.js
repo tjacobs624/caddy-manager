465cbb07219c5a3350d89e2aa6dd573d7c4d216f
Tim Jacobs <80988888+tjacobs624@users.noreply.github.com>
2025-05-04 02:30:58 +0000

    Add Webpack configuration and bundle JavaScript for production

---
+++ caddy-manager/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    entry: './public/script.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    mode: 'production',
+};
